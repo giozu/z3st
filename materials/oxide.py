d6e850cd0301e7e08a562de48e2c16bcb660872c
giozu <giovanni.zullo@polimi.it>
2025-11-14 12:13:07 +0100

    update

--- materials/oxide.py
+++ materials/oxide.py
@@ -7,5 +7,7 @@ def k(T):
     Returns:
         k (W/m-K): Thermal conductivity as a UFL expression
     """
+    # return 1.0 * ufl.exp(-0.01 * T)
+    # return 10 + 0.02*T + 5e-4*T**2
 
-    return 2.5
+    return 2.5 / (T*1e-6 + 1.0)
