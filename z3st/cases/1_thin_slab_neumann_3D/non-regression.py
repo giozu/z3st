51cf274a2ea538e96688b6d02535e7b5104042c4
giozu <giovanni.zullo@polimi.it>
2026-02-14 18:57:21 +0100

    refactor: load geometry and material properties from YAML files in non-regression scripts and remove axisymmetric flag.

--- z3st/cases/1_thin_slab_neumann_3D/non-regression.py
+++ z3st/cases/1_thin_slab_neumann_3D/non-regression.py
@@ -9,8 +9,8 @@ Steady-state 3D slab (Neumann-Dirichlet).
 
 """
 
-import os
-
+import os, re
+import yaml
 import numpy as np
 
 from z3st.utils.utils_extract_vtu import *
@@ -21,38 +21,47 @@ from z3st.utils.utils_verification import *
 CASE_DIR = os.path.dirname(__file__)
 VTU_FILE = os.path.join(CASE_DIR, "output", "fields.vtu")
 OUT_JSON = os.path.join(CASE_DIR, "output", "non-regression.json")
+OUTPUT_DIR = os.path.join(CASE_DIR, "output")
+MATERIAL_FILE = os.path.join(CASE_DIR, "../../materials/vessel_steel_0.yaml")
+GEOMETRY_FILE = os.path.join(CASE_DIR, "geometry.yaml")
+MESH_GEO_FILE = os.path.join(CASE_DIR, "mesh.geo")
 
-# Geometry and material
-Lx, Ly, Lz = 0.100, 2.0, 2.0  # m (geometry dimensions)
-k, E, nu, alpha = (
-    48.1,
-    1.77e11,
-    0.3,
-    1.7e-5,
-)  # W/m·K, Pa, -, 1/K (thermal conductivity, Young's modulus, Poisson's ratio, thermal expansion)
+# Geometry, material, boundary conditions:
+with open(GEOMETRY_FILE, 'r') as f:
+    geom_data = yaml.safe_load(f)
+Lx = float(geom_data.get('Lx'))
+Ly = float(geom_data.get('Ly'))
+Lz = float(geom_data.get('Lz'))
+
+with open(MATERIAL_FILE, 'r') as f:
+    mat_data = yaml.safe_load(f)
+E  = float(mat_data.get('E'))
+nu = float(mat_data.get('nu'))
+k  = float(mat_data.get('k'))
+alpha = float(mat_data.get('alpha'))
+
+with open(MESH_GEO_FILE, 'r') as f:
+    content = f.read()
+nx = float(re.search(rf'nx\s*=\s*([\d\.]+);', content).group(1))
+ny = float(re.search(rf'ny\s*=\s*([\d\.]+);', content).group(1))
+
+print(f"[INFO] Geometry loaded: Lx = {Lx} m, Ly = {Ly} m, Lz = {Lz} m")
+print(f"[INFO] Material loaded: E = {E:.2e} Pa, nu = {nu}, k = {k:.2e} W/m·K, alpha = {alpha:.2e} 1/K")
+
 Ti, To = 573.0, 583.0  # K (boundary temperature)
-q0, mu = 0.00, 24.0  # W/m³, 1/m (volumetric heat source, attenuation coefficient)
-y_target, z_target, mask_tol = Ly / 2, Lz / 2, 0.65  # m, m, m (plane selection and tolerance)
-
+y_target, z_target, mask_tol = Ly / 2, Lz / 2, Ly/(ny-1)/2  # m, m, m (plane selection and tolerance)
 TOLERANCE = 3e-3  # - (relative tolerance for non-regression tests)
-
 
 # --.. ..- .-.. .-.. --- analytic functions  --.. ..- .-.. .-.. ---
 def analytic_T(x):
     """Analytical temperature profile (slab)."""
     term1 = Ti + (To - Ti) * (x / Lx)
-    term2 = (q0 / (mu**2 * k)) * ((x / Lx) * (np.exp(-mu * Lx) - 1) - (np.exp(-mu * x) - 1))
-    return term1 + term2
-
+    return term1
 
 def sigma_th(x, T_num, c=1.0):
     """Thermal stress profile (linear)."""
     T_mean = np.trapezoid(T_num, x) / (x.max() - x.min())
     return alpha * E / (1.0 - c * nu) * (T_mean - T_num)
-
-
-# --.. ..- .-.. .-.. --- checks --.. ..- .-.. .-.. ---
-list_fields(VTU_FILE)
 
 # --.. ..- .-.. .-.. --- results --.. ..- .-.. .-.. ---
 print(f"[INFO] Target y-plane for extraction: y = {y_target:.4e} m")
